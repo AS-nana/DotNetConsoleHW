c6033e7e2bf9cc9e137b5cdca424edcd8bbadf98
Adriana Socaciu <adriana.socaciu28@gmail.com>
2024-11-27 15:29:12 +0200

    age not right

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello!");
+
+Console.WriteLine("Please input your First name: ");
+string fname = Console.ReadLine();
+Console.WriteLine("Please input your Last name: ");
+string lname = Console.ReadLine();
+Console.WriteLine("Please input your age: ");
+int age = Console.ReadLine();
+Console.WriteLine("Please input your gender: ");
+string gender = Console.ReadLine();
+
+int ageIn20 = age + 20;
 
 
-string name = Console.ReadLine();
-Console.WriteLine("Your name is " + name);
+
+Console.WriteLine("\nYour details are as follows: ");
+Console.WriteLine($"- Fist name: " + fname);
+Console.WriteLine($"- Last Name: " + lname);
+Console.WriteLine($"- Gender: "+ gender);
+Console.WriteLine("You will be" + ageIn20 + "years old in 20 years!");
